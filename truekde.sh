1f16fcc4b9316da59432d699397e0831165435bf
88 <104312143+LechTheMitch@users.noreply.github.com>
2024-09-08 02:54:32 +0300

    Remove Time Wasters

--- truekde.sh
+++ truekde.sh
@@ -6,22 +6,19 @@ cd /tmp
 
 git clone https://aur.archlinux.org/paru-bin.git && cd paru-bin && makepkg -si
 
-paru -S --needed flatpak flatpak-kcm xdg-desktop-portal-gtk plymouth kdeconnect xwaylandvideobridge nix virt-manager unrar p7zip unarchiver lzop lrzip arj firefox okular gimp packagekit-qt6 snapd qemu-desktop ttf-dejavu noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra acer-wmi-battery-dkms vmware-workstation power-profiles-daemon supergfxctl plasma6-applets-supergfxctl looking-glass dnsmasq swtpm waydroid distrobox podman kio-admin sbctl spectacle cups system-config-printer fwupd pacutils pacman-contrib appmenu-gtk-module kio-gdrive gwenview filelight sshfs nbfc-linux kcalc zsh xmlstarlet jq unzip local-by-flywheel-bin teamviewer kdepim-addons vulkan-intel partitionmanager kdegraphics-thumbnailers ffmpegthumbs qt6-imageformats kimageformats switcheroo-control fzf cryfs encfs gocryptfs lsb-release klassy-bin kf6-servicemenus-reimage proton-vpn-gtk-app davinci-resolve-studio qt5-tools
+paru -S --needed --noconfirm flatpak flatpak-kcm xdg-desktop-portal-gtk plymouth kdeconnect xwaylandvideobridge nix virt-manager unrar p7zip unarchiver lzop lrzip arj firefox okular gimp packagekit-qt6 snapd qemu-desktop ttf-dejavu noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra acer-wmi-battery-dkms vmware-workstation power-profiles-daemon supergfxctl plasma6-applets-supergfxctl looking-glass dnsmasq swtpm waydroid distrobox podman kio-admin sbctl spectacle cups system-config-printer fwupd pacutils pacman-contrib appmenu-gtk-module kio-gdrive gwenview filelight sshfs nbfc-linux kcalc zsh xmlstarlet jq unzip local-by-flywheel-bin teamviewer kdepim-addons vulkan-intel partitionmanager kdegraphics-thumbnailers ffmpegthumbs qt6-imageformats kimageformats switcheroo-control fzf cryfs encfs gocryptfs lsb-release klassy-bin kf6-servicemenus-reimage proton-vpn-gtk-app davinci-resolve-studio opencl-nvidia jhead
 
 
 echo -e "\nInstalling Flatpaks\n"
-flatpak install org.kde.kdenlive org.kde.krita org.libreoffice.LibreOffice com.discordapp.Discord com.google.Chrome com.obsproject.Studio it.mijorus.gearlever io.github.giantpinkrobots.flatsweep us.zoom.Zoom io.github._0xzer0x.qurancompanion com.usebottles.bottles com.heroicgameslauncher.hgl org.kde.skanpage net.pcsx2.PCSX2 com.valvesoftware.Steam org.DolphinEmu.dolphin-emu org.freedesktop.Platform.VulkanLayer.MangoHud com.atlauncher.ATLauncher com.anydesk.Anydesk org.ppsspp.PPSSPP com.github.unrud.VideoDownloader org.gnome.Epiphany com.boxy_svg.BoxySVG org.gnome.World.PikaBackup page.kramo.Cartridges
+flatpak install org.kde.kdenlive org.kde.krita org.libreoffice.LibreOffice com.discordapp.Discord com.google.Chrome com.obsproject.Studio it.mijorus.gearlever io.github.giantpinkrobots.flatsweep us.zoom.Zoom io.github._0xzer0x.qurancompanion com.usebottles.bottles com.heroicgameslauncher.hgl org.kde.skanpage com.anydesk.Anydesk com.github.unrud.VideoDownloader org.gnome.Epiphany com.boxy_svg.BoxySVG org.gnome.World.PikaBackup
 
 echo -e "\nSetting Important Flatpak overrides\n"
 flatpak override --user --filesystem=~/.local/share/applications:create --filesystem=~/.local/share/icons:create
 flatpak override --user --filesystem=xdg-config/gtk-3.0:ro
-flatpak override --user com.usebottles.bottles --filesystem=~/.var/app/com.valvesoftware.Steam/data/Steam
 flatpak override com.usebottles.bottles --user --filesystem=xdg-data/applications
 flatpak override --user --env=XDG_SESSION_TYPE=x11 com.discordapp.Discord
-flatpak override --user --env=STEAM_FORCE_DESKTOPUI_SCALING=1.5 com.valvesoftware.Steam
 flatpak override --user --env=QT_SCALE_FACTOR=1.5 us.zoom.Zoom
 
-paru -R qt5-tools
 
 cp ~/.dotfiles/TrueKDE/kglobalshortcutsrc ~/.config/
 cp ~/.dotfiles/TrueKDE/kwinrulesrc ~/.config/
@@ -44,6 +41,9 @@ sudo systemctl enable snapd.socket
 sudo systemctl enable snapd.apparmor.service
 sudo systemctl enable nbfc_service
 sudo systemctl enable switcheroo-control
+sudo systemctl enable nvidia-suspend.service
+sudo systemctl enable nvidia-hibernate.service
+sudo systemctl enable nvidia-resume.service
 
 echo -e "\nEnabling Secure Boot\n"
 sudo sbctl create-keys
